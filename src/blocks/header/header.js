ca7d4db5a6fd5a75a87496216917666d8dbe2fa9
motosasha <moto.sasha@gmail.com>
2024-09-10 20:54:37 +0300

    feat: — swap burger to phone link

--- src/blocks/header/header.js
+++ src/blocks/header/header.js
@@ -1,15 +1,9 @@
 import { ready } from "../../js/utils/documentReady.js";
-import { openNav } from "../../js/common/openNav.js";
 
 ready(function () {
   const pageHeader = document.querySelector(".header");
 
   if (pageHeader) {
-    const headerBurger = pageHeader.querySelector(".header__burger");
-    headerBurger.addEventListener("click", () => {
-      openNav(document.body, "open-nav");
-    });
-
     const headerLogo = pageHeader.querySelector(".header__logo");
     headerLogo.addEventListener("click", () => {
       window.scrollTo({ top: 0, behavior: "smooth" });
